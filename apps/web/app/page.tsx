ab6a4b4dd567fc549f130eae3a0c34adc2cd8377
Amit Saha <asaha.bwn2000@gmail.com>
2023-12-25 02:14:46 +0530

    error!!

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,22 @@
 'use client'
-import classes from "./page.module.css"
+import { useState } from "react";
+import { useSocket } from "../context/SocketProvider";
+import classes from "./page.module.css";
 
-export default function page(){
+export default function Page(){
+
+  const { sendMessage } = useSocket();
+  const [message, setMessage] = useState("");
+
   return (
     <div>
       <div>
         <h1>All messages</h1>
       </div>
       <div>
-        <input className={classes["chat-input"]} type="text" placeholder="Message..." />
-        <button className={classes["button"]}>Send</button>
+        <input onChange={(e) => setMessage(e.target.value)} className={classes["chat-input"]} type="text" placeholder="Message..." />
+
+        <button onClick={(e) => sendMessage(message)} className={classes["button"]}>Send</button>
       </div>
     </div>
   );
--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import React, { useCallback, useContext, useEffect } from "react";
-import { io } from "socket.io-client";
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import { io, Socket } from "socket.io-client";
 
 interface SocketProviderProps{
     children ?: React.ReactNode
@@ -18,10 +18,11 @@ export const useSocket = () => {
     if (!state) throw new Error(`state is undefined`);
 
     return state;
-}
+};
 
 
 export const SocketProvider: React.FC<SocketProviderProps> = ({children}) => {
+    const [socket, setSocket] = useState<Socket>();
 
     const sendMessage: ISocketContext["sendMessage"] = useCallback((msg) => {
         console.log(`Sent message `, msg);
@@ -29,9 +30,11 @@ export const SocketProvider: React.FC<SocketProviderProps> = ({children}) => {
 
     useEffect(() => {
         const _socket = io("http://localhost:8000");
+        setSocket(_socket);
 
         return () => {
             _socket.disconnect();
+            setSocket(undefined);
         }
     }, []);
 
